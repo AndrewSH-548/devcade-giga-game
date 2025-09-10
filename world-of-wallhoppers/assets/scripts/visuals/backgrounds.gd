390689bb8a7e20c347bf3ca9c4557e8d64273304
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-09-10 18:11:02 -0400

    Added Parallax System and Initial Backgrounds
    A "Backgrounds" Node is now added to the Volcano Level. Handles Parallax layers for the level. Added Global Groups "Player1Camera" and "Player2Camera". Sub viewports for Player 1 and Player 2 no longer render visibility layers 4 and 3 respectively. This is needed for parallax, and shouldn't affect anything, but do message me if there are things not rendering.

---
+++ world-of-wallhoppers/assets/scripts/visuals/backgrounds.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+const PLAYER_1_VISIBILITY: int = 4 # Visibility Layer 3
+const PLAYER_2_VISIBILITY: int = 8 # Visibility Layer 4
+
+var player_1_camera: Camera2D
+var player_2_camera: Camera2D
+
+# Used to differentiate the backgrounds
+var player_1_backgrounds: Array[Parallax2D]
+
+## This node should be setup with all parallax layers as direct children.
+
+func _ready() -> void:
+	player_1_camera = get_tree().get_first_node_in_group("Player1Camera")
+	player_2_camera = get_tree().get_first_node_in_group("Player2Camera")
+		
+	for background in get_children():
+		if background is not Parallax2D: continue 
+		player_1_backgrounds.append(background)
+		# Make this layer only visible to player 1. This is required for parallax
+		background.visibility_layer = PLAYER_1_VISIBILITY
+		background.ignore_camera_scroll = true
+		# Skip the rest if in singleplayer
+		if player_2_camera == null: continue
+		# Duplicate each parallax node fully
+		var secondary_background: Parallax2D = background.duplicate()
+		add_child(secondary_background)
+		secondary_background.global_position = background.global_position
+		# Make this layer only visible to player 2. This is required for parallax
+		secondary_background.visibility_layer = PLAYER_2_VISIBILITY
+		#secondary_background.follow_viewport = false
+		secondary_background.ignore_camera_scroll = true
+
+func _process(delta: float) -> void:
+	for background in get_children():
+		if background is not Parallax2D: continue 
+		if background in player_1_backgrounds:background.screen_offset.y = player_1_camera.get_screen_center_position().y
+		else: background.screen_offset.y = player_2_camera.get_screen_center_position().y
