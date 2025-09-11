0f19bc9277b4442979cdd779231569f15cea7e6e
ShadowArcher289 <ahasuncion21@gmail.com>
2025-09-11 08:44:48 -0400

    Goalpost Updated
    Added a sprite to the goalpost and it works!
    
    Also edited levels.gd in last push to set player bodies 1 & 2 to their respective groups.

--- world-of-wallhoppers/assets/scripts/goalpost.gd
+++ world-of-wallhoppers/assets/scripts/goalpost.gd
@@ -5,13 +5,15 @@ extends Node2D
 ## one parameter: the player number (an integer)
 var game_finish: Signal
 
+
 ## When one of the player(s) hits the goal, the game should end. [br]
 ## This function handles the emitition of the signal of that [br]
 ## parameter [b]player_number[/b]: the player number that hit/entered a goal area
 func _on_goal_1_body_entered(body: Node2D) -> void:
-	if body == CharacterBody2D && body.is_in_group("player1"): # player 1
+	if body.is_in_group("player1"): # player 1 (node type doesn't matter, just has to be in group player 1)
+		print("RUNNING")
 		end_game(1)
-	if body == CharacterBody2D && body.is_in_group("player2"): # player2
+	if body.is_in_group("player2"): # player2 (node type doesn't matter, just has to be in group player 2)
 		end_game(2)
 
 func end_game(player_number: int) -> void:
