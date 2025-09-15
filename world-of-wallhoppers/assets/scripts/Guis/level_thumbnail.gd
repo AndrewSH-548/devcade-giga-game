2efc28e08de556bdc3ecce8ff2d1ff711dcb8489
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-09-15 17:45:17 -0400

    Various Visual Upgrades

--- world-of-wallhoppers/assets/scripts/Guis/level_thumbnail.gd
+++ world-of-wallhoppers/assets/scripts/Guis/level_thumbnail.gd
@@ -1,7 +1,8 @@
-extends Control
+extends Button
 
 @onready var level_viewport: SubViewport = $ViewportContainer/LevelViewport
 @onready var level_name: Label = $Margins/LevelName
+@onready var shading: ColorRect = $Shading
 
 const SHOWCASE_SPEED: float = 64.0
 
@@ -16,6 +17,7 @@ func setup(level_info: LevelInfo):
 	var level_scene: PackedScene = level_info.scene
 	level_name.text = level_info.name
 	is_setup = true
+	assert(level_info.scene != null, "LevelInfo cannot be loaded with no level scene!")
 	# Instantiate level and add it so the SubViewport
 	level = level_scene.instantiate()
 	level.process_mode = Node.PROCESS_MODE_DISABLED
@@ -39,6 +41,10 @@ func get_child_in_group(parent: Node, group: StringName) -> Node:
 	return null
 
 func _process(delta: float) -> void:
+	shading.visible = not is_hovered() and not get_viewport().gui_get_focus_owner() == self
+	if not is_hovered() and not get_viewport().gui_get_focus_owner() == self:
+		level.global_position = -thumbnail_marker.global_position
+		return
 	if backgrounds_manager != null:
 		backgrounds_manager.manual_set_camera_position(-level.global_position)
 	
