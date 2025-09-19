1912b5dae64d3a0305e5f4103bb616751923e37f
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-09-18 21:10:48 -0400

    Initial Portrait Class and Resource

---
+++ world-of-wallhoppers/assets/scripts/Guis/character_select/character_portrait.gd
@@ -0,0 +1,4 @@
+extends Control
+class_name CharacterPortrait
+
+@export var info: CharacterPortraitInfo
---
+++ world-of-wallhoppers/assets/scripts/Guis/character_select/character_portrait_info.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name CharacterPortraitInfo
+
+@export var character_name: String
+@export var portrait_sprite: Texture2D
