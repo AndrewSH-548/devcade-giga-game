42d455b8227cf0871ef1b7bda0692e08469e8928
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-09-19 23:01:16 -0400

    Pre-Rework Commit

--- world-of-wallhoppers/assets/scripts/Guis/character_select/character_portrait.gd
+++ world-of-wallhoppers/assets/scripts/Guis/character_select/character_portrait.gd
@@ -6,6 +6,8 @@ class_name CharacterPortrait
 @onready var character_name: Label = $PanelContainer/MainMargins/Vertical/NamePanel/Name
 
 var self_index: int = 0
+# In Radians (0 -> TAU or 2PI)
+var rotation_position: float = 0
 
 func setup(info: CharacterPortraitInfo) -> void:
 	element_icon.texture = info.element_sprite
--- world-of-wallhoppers/assets/scripts/Guis/character_select/character_select.gd
+++ world-of-wallhoppers/assets/scripts/Guis/character_select/character_select.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @export var characters: Array[CharacterPortraitInfo]
-@onready var character_wheel: MarginContainer = $MainVertical/CharacterWheel
+@onready var character_wheel: Control = $MainVertical/CharacterWheel
 const CHARACTER_PORTRAIT = preload("uid://br12fqlynos76")
 
 var current_selected: int = 0:
@@ -9,13 +9,15 @@ var current_selected: int = 0:
 		current_selected = new_value
 		current_selected = wrap(current_selected, 0, portraits.size())
 
-var portrait_spacing: float = 250
-var rotate_speed: float = 500
+var portrait_spacing: Vector2 = Vector2(500, 20)
+var carousel_offset: Vector2 = Vector2(0, 200)
+var rotate_speed: float = 2
 
 var portraits: Array[CharacterPortrait] = []
 
 func _ready() -> void:
-	var index: int = 0
+	var index: int = portraits.size()
+	
 	for character in characters:
 		
 		var portrait: CharacterPortrait = CHARACTER_PORTRAIT.instantiate()
@@ -23,13 +25,12 @@ func _ready() -> void:
 		portrait.setup(character)
 		portrait.self_index = index
 		portraits.append(portrait)
-		portrait.global_position.x = get_portrait_target_x(portrait)
 		
 		index += 1
+	
+	for portrait in portraits:
+		move_portait_carousel_position(portrait)
 
-func get_portrait_target_x(portrait: CharacterPortrait) -> float:
-	return (portrait.self_index - current_selected) * portrait_spacing + DisplayServer.window_get_size().x / 2.0 - portrait.size.x / 2.0
-#Did some work on the character select! There's still a lot to be done, but I've got the visuals mostly down. What do you think o
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_left"):
 		current_selected -= 1
@@ -39,26 +40,24 @@ func _input(event: InputEvent) -> void:
 func sincos_vector(value: float) -> Vector2:
 	return Vector2(sin(value), cos(value))
 
+func portait_offset(portrait: CharacterPortrait) -> Vector2:
+	return Vector2(-portrait.size.x / 2.0 + get_viewport_rect().size.x / 2.0, 0)
+
+func move_portait_carousel_position(portrait: CharacterPortrait, process_delta: float = -1) -> void:
+	var index_distance: int = wrap(current_selected - portrait.self_index, 0, portraits.size())
+	var center_rotation: float = (float(index_distance) / int(portraits.size()) * TAU)
+	
+	if process_delta == -1:
+		portrait.rotation_position = center_rotation
+	else:
+		portrait.rotation_position = rotate_toward(portrait.rotation_position, center_rotation, rotate_speed * process_delta * (10.0 / portraits.size()))
+	
+	portrait.z_index = int((1.0 - (float(abs(wrapf(portrait.rotation_position, -PI, PI))) / TAU)) * 10) + 1
+	portrait.modulate = Color.from_hsv(0.0, 0.0, (1.0 - (float(abs(wrapf(portrait.rotation_position, -PI, PI))) / TAU)))
+	
+	portrait.scale = Vector2.ONE * (1.0 - (float(abs(wrapf(portrait.rotation_position, -PI, PI))) / TAU))
+	portrait.global_position = sincos_vector(portrait.rotation_position) * Vector2(-1, 1) * portrait_spacing + portait_offset(portrait) + carousel_offset
+
 func _process(delta: float) -> void:
 	for portrait: CharacterPortrait in portraits:
-		
-		var index_distance: int = wrap(current_selected - portrait.self_index, 0, portraits.size())
-		
-		var center_rotation: float = (float(index_distance) / int(portraits.size()) * TAU)
-		
-		portrait.global_position = portrait.global_position.move_toward(sincos_vector(center_rotation) * portrait_spacing, delta * rotate_speed)
-		
-		continue
-		
-		if portrait.self_index == current_selected: portrait.z_index = 2
-		else: portrait.z_index = 1
-		
-		portrait.global_position.x = move_toward(portrait.global_position.x,
-			get_portrait_target_x(portrait),
-			delta * rotate_speed
-		)
-		
-		if abs(portrait.global_position.x) < 1000:
-			var distance: float = abs(DisplayServer.window_get_size().x / 2.0 - portrait.size.x / 2.0 - portrait.global_position.x)
-			portrait.scale.x = 1.0 / pow(1.001, distance)
-			portrait.scale.y = 1.0 / pow(1.001, distance)
+		move_portait_carousel_position(portrait, delta)
