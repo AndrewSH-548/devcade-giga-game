fb00f7204c1ad407f1753ca8b9792feb7d0df491
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-09-28 17:51:18 -0400

    Added Large Portraits and Portrait Holders

---
+++ world-of-wallhoppers/assets/scripts/Guis/character_select/large_portrait_holder.gd
@@ -0,0 +1,24 @@
+extends Node2D
+class_name PortraitHolder
+
+@onready var character_select: CharacterSelect = get_tree().get_first_node_in_group("CharacterSelect")
+
+enum PLACEMENT {
+	TOP,
+	BOTTOM,
+}
+
+@export var placement: PLACEMENT
+@export var connected_dial: CharacterSelect.DIAL
+
+var connected_player_index: int
+
+func _ready() -> void:
+	connected_player_index = 0 if placement == PLACEMENT.TOP else 1
+	assert(character_select != null, "Character select not found for a portrait holder! Is the character select not in the \"CharacterSelect\" group?")
+
+func _process(delta: float) -> void:
+	if character_select.current_dial_selection[connected_player_index] == connected_dial:
+		visible = true
+	else:
+		visible = false
