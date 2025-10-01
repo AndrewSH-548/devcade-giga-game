39e287ba926a391837eda6e8b8846124ea27d588
ShadowArcher289 <ahasuncion21@gmail.com>
2025-10-01 19:59:08 -0400

    Added a timer that increments in 0.1, 1 every second.

--- world-of-wallhoppers/assets/scripts/singlescreen.gd
+++ world-of-wallhoppers/assets/scripts/singlescreen.gd
@@ -1,6 +1,10 @@
 extends MainLevelHeader
 
 @onready var player_1_body: CharacterBody2D = $ViewportContainerP1/SubViewport/Player1Body
+@onready var timer_label: Label = $ViewportContainerP1/SubViewport/Camera2D/TimerLabel
+@onready var singleplayer_timer: Timer = $ViewportContainerP1/SubViewport/SingleplayerTimer
+
+var singleplayer_time: float = 0;
 
 func _ready() -> void:
 	player_1_body.add_to_group("player1");
@@ -9,3 +13,8 @@ func _ready() -> void:
 func place_level(level: Node2D) -> void:
 	$ViewportContainerP1/SubViewport.add_child(level)
 	
+
+
+func _on_singleplayer_timer_timeout() -> void:
+	singleplayer_time += 1;
+	timer_label.text = str(singleplayer_time/10);
