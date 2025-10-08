bdf9ba3c87378417633eabc4a87a2df0ce4f362d
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-10-08 18:33:58 -0400

    Sprite and Tracks Implemented

--- world-of-wallhoppers/assets/scripts/moving_platform.gd
+++ world-of-wallhoppers/assets/scripts/moving_platform.gd
@@ -28,12 +28,16 @@ extends Node2D
 @export var pause_area_radius: float = 8; ## The minimum distance from the pause area that will cause the platform to pause.
 @export var pause_duration: float = 0.8; ## The time(in seconds) the platform will stay paused.
 
-@export var speed_scale: float = 0.6; ## The speed scaling ratio. For example, if this value is 1, then the platform moves at normal speed. If it's 0.5, then it moves at half speed. If it's 2, then it moves at double speed.
-@export var path_type: String = "BOUNCE"; ## The type of path the platform will follow. [BOUNCE] or [LOOP]. For [BOUNCE] the platform will travel start>end, end> start. For [LOOP] the platform will travel start>end, start>end continuously.
+@export var speed_scale: float = 1.0; ## The speed scaling ratio. For example, if this value is 1, then the platform moves at normal speed. If it's 0.5, then it moves at half speed. If it's 2, then it moves at double speed.
+@export var path_type: Path_System = Path_System.BOUNCE; ## The type of path the platform will follow. [BOUNCE] or [LOOP]. For [BOUNCE] the platform will travel start>end, end> start. For [LOOP] the platform will travel start>end, start>end continuously.
+
+const MOVING_PLATFORM_PATH = preload("uid://k6238xtiriwm") ## Scene which contains the visuals for a single path part. These are constructed using the moving platform's path points
+const MOVING_PLATFORM_REST = preload("uid://yx5v3vnlt8nw") ## Scene which contains the visuals for a rest point
 
 var paused = false; ## If the platform is paused. Then this is true
 var pause_enabled = true; ## Stores if the pause_areas are enabled or not.
-	
+var speed_factor: float = 1.0 ## Used internally. This is the inverse of the length of the platform's path. Used to keep the platform's speed consistent with different length paths
+
 enum Path_System { ## different states for the path behavior
 	BOUNCE, ## travel from start->end, then end->start
 	LOOP, ## travel in a loop: start->end, start->end
@@ -54,32 +58,78 @@ func _ready() -> void:
 	if (pauses == null): # if the pauses array is empty, then disable pause
 		pause_enabled = false;
 
-	# set the position of the path points (subtract self.global_position to set the points at the right position)
-	path_2d.curve.set_point_position(0, Vector2(start_point_0.global_position.x - self.global_position.x, start_point_0.global_position.y - self.global_position.y));
-	path_2d.curve.set_point_position(1, Vector2(end_point.global_position.x - self.global_position.x, end_point.global_position.y - self.global_position.y));
+	# set the position of the path points (use to_local on the nodes's globals positions for correct points)
+	path_2d.curve.set_point_position(0, to_local(start_point_0.global_position));
+	path_2d.curve.set_point_position(1, to_local(end_point.global_position));
 
 	for i in range(len(points)): # sets the points to their assigned positions
-		path_2d.curve.add_point(Vector2(points[i].global_position.x - self.global_position.x, points[i].global_position.y - self.global_position.y), Vector2(0, 0), Vector2(0, 0), i+1);
-
-
-	match path_type.to_upper(): # set the path type based on the variable
-		"LOOP":
-			current_state = Path_System.LOOP;
-		"BOUNCE":
-			current_state = Path_System.BOUNCE;
-		_:
-			print_debug("ERROR: Please Input LOOP or BOUNCE for the path type.");
+		path_2d.curve.add_point(to_local(points[i].global_position), Vector2(0, 0), Vector2(0, 0), i+1);
+	
+	current_state = path_type
+	
+	# Scale the speed of the platform based on path length
+	# This makes the platform's speed always consistent
+	speed_factor = 1.0 / path_2d.curve.get_baked_length()
 	
 	if current_state == Path_System.BOUNCE: # run BOUNCE path system
-		animation_player.speed_scale = speed_scale; # set the speed_scale of the platform for (BOUNCE)
+		animation_player.speed_scale = speed_scale * speed_factor * 128.0; # set the speed_scale of the platform for (BOUNCE)
 		animation_player.play("move_platform");
 	elif current_state == Path_System.LOOP:
 		pass
+	
+	# Next the visual tracks and rests will be placed
+	# Wait one frame, because we need to let the parent node setup it's children
+	await get_tree().process_frame
+	
+	# Get the (very large) array of points in the curve
+	# Note that this is NOT the points we chose, instead it's hundreds of points which form the curve
+	# that is CREATED by the points we chose
+	var points: PackedVector2Array = path_2d.curve.get_baked_points()
+	# Will store the last position a track was placed
+	var last_position: Vector2
+	# Will store the last track node what was placed
+	var last_track: NinePatchRect = null
+	# For each point
+	for index in range(points.size()):
+		# If it's the last point...
+		if index >= points.size() - 1:
+			# Looping
+			if current_state == Path_System.LOOP:
+				index = 0
+				# Currently looping is teleporting, but if it ever becmes a smooth loop, just delete this BREAK statement below:
+				break
+			# Bouncing
+			else:
+				# End the loop
+				break
+		# Get current point and next point, and add randomness to current for visual flair
+		var point: Vector2 = points[index] + Vector2(randi_range(-2, 2), randi_range(-2, 2)) - Vector2(0, 4)
+		var next_point: Vector2 = points[index + 1]
+		# Only place tracks a certain distance aprart
+		# This stops hundreds of tracks being placed
+		if index == 0 or last_position.distance_to(point) > 32:
+			# If there was a last track, adjust it to connect to this track
+			if last_track != null:
+				last_track.size.x = last_position.distance_to(point)
+				last_track.rotation = last_position.angle_to_point(point)
+			# Save the current position
+			last_position = point
+			# Create the new track
+			var path_visual: NinePatchRect = MOVING_PLATFORM_PATH.instantiate()
+			get_parent().add_child(path_visual)
+			# Position the new track and save it for the next track
+			path_visual.global_position = global_position + point
+			path_visual.size.x = 0
+			last_track = path_visual
+	
+	for pause in pauses:
+		var position: Vector2 = pause.global_position
+		
 
 
 func _process(delta: float) -> void:
 	if !paused && (current_state == Path_System.LOOP): # run LOOP path system
-		path_follow_2d.progress += (10 * speed_scale); # continuously increase the platform's path progression (LOOPs)
+		path_follow_2d.progress += (128 * 60 * 10 * speed_scale * speed_factor * delta); # continuously increase the platform's path progression (LOOPs)
 
 	if !paused && pause_cooldown_timer.is_stopped(): # pause system. pauses the platform at the pause_area for PLATFORM_PAUSE_DELAY seconds
 		if is_touching_pause_area():
