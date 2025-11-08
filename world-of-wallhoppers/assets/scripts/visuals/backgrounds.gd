4dfad64c07d9e4458be0335ec4372e1a4abaf53c
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-11-08 15:03:19 -0500

    Starting to Rework Backgrounds

--- world-of-wallhoppers/assets/scripts/visuals/backgrounds.gd
+++ world-of-wallhoppers/assets/scripts/visuals/backgrounds.gd
@@ -6,6 +6,7 @@ const PLAYER_2_VISIBILITY: int = 8 # Visibility Layer 4
 const SCROLL_SCALE_GLOBAL: float = 0.5
 
 @export var scroll_scale_local: float = 1.0
+@export var extra_background_holders: Array[Node]
 
 var player_1_camera: Camera2D
 var player_2_camera: Camera2D
@@ -23,22 +24,28 @@ func _ready() -> void:
 	player_2_camera = get_tree().get_first_node_in_group("Player2Camera")
 		
 	for background in get_children():
-		if background is not Parallax2D: continue 
-		player_1_backgrounds.append(background)
-		# Make this layer only visible to player 1. This is required for parallax
-		background.visibility_layer = PLAYER_1_VISIBILITY
-		background.ignore_camera_scroll = true
-		# Skip the rest if in singleplayer
-		if player_2_camera == null: continue
-		# Duplicate each parallax node fully
-		var secondary_background: Parallax2D = background.duplicate()
-		add_child(secondary_background)
-		player_2_backgrounds.append(secondary_background)
-		secondary_background.global_position = background.global_position
-		# Make this layer only visible to player 2. This is required for parallax
-		secondary_background.visibility_layer = PLAYER_2_VISIBILITY
-		#secondary_background.follow_viewport = false
-		secondary_background.ignore_camera_scroll = true
+		setup_background(background)
+	for extra in extra_background_holders:
+		for background in extra.get_children():
+			setup_background(background)
+
+func setup_background(background: Node):
+	if background is not Parallax2D: return
+	player_1_backgrounds.append(background)
+	# Make this layer only visible to player 1. This is required for parallax
+	background.visibility_layer = PLAYER_1_VISIBILITY
+	background.ignore_camera_scroll = true
+	# Skip the rest if in singleplayer
+	if player_2_camera == null: return
+	# Duplicate each parallax node fully
+	var secondary_background: Parallax2D = background.duplicate()
+	add_child(secondary_background)
+	player_2_backgrounds.append(secondary_background)
+	secondary_background.global_position = background.global_position
+	# Make this layer only visible to player 2. This is required for parallax
+	secondary_background.visibility_layer = PLAYER_2_VISIBILITY
+	#secondary_background.follow_viewport = false
+	secondary_background.ignore_camera_scroll = true
 
 func manual_set_camera_position(camera_position: Vector2):
 	for background in get_children():
@@ -47,8 +54,7 @@ func manual_set_camera_position(camera_position: Vector2):
 
 func _process(_delta: float) -> void:
 	if thumbnail_mode: return
-	for background in get_children():
-		if background is not Parallax2D: continue 
-		if background in player_1_backgrounds:
-			background.screen_offset.y = player_1_camera.get_screen_center_position().y * SCROLL_SCALE_GLOBAL * scroll_scale_local
-		else: background.screen_offset.y = player_2_camera.get_screen_center_position().y * SCROLL_SCALE_GLOBAL * scroll_scale_local
+	for background in player_1_backgrounds:
+		background.screen_offset.y = player_1_camera.get_screen_center_position().y * SCROLL_SCALE_GLOBAL * scroll_scale_local
+	for background in player_2_backgrounds:
+		background.screen_offset.y = player_2_camera.get_screen_center_position().y * SCROLL_SCALE_GLOBAL * scroll_scale_local
