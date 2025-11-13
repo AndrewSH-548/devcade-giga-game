c4ae4304ef4813380b7e495effd60eab817cc5b3
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-11-13 11:46:12 -0500

    Split Win Scene into Two Parts

--- world-of-wallhoppers/assets/scripts/win_scene.gd
+++ world-of-wallhoppers/assets/scripts/win_scene.gd
@@ -1,24 +1,31 @@
 extends Control
 
-@onready var time_label: Label = $VBoxContainer/TimeLabel
-@onready var leaderboard_title: Label = $VBoxContainer/LeaderboardTitle
-@onready var leaderboard: Label = $VBoxContainer/Leaderboard
+@onready var time_label: Label = $Results/TimeLabel
+@onready var leaderboard_title: Label = $Results/LeaderboardTitle
+@onready var leaderboard: Label = $Results/Leaderboard
+@onready var results: VBoxContainer = $Results
 
-@onready var name_input: Button = $"VBoxContainer/Name Input"
-@onready var player_name_label: Label = $NameInputPanel/NameInputContainer/Panel/PlayerNameLabel
-@onready var name_input_container: VBoxContainer = $NameInputPanel/NameInputContainer
-@onready var name_input_panel: Panel = $NameInputPanel
+@onready var name_input: Button = $"Results/Name Input"
+@onready var player_name_label: Label = $EnterRecord/NameInputContainer/Panel/PlayerNameLabel
+@onready var name_input_container: VBoxContainer = $EnterRecord/NameInputContainer
+@onready var name_input_panel: Panel = $EnterRecord
+@onready var d_key: Button = $EnterRecord/NameInputContainer/GridContainer/D
 
-@onready var level_select_button: Button = $"VBoxContainer/Level Select"
+@onready var level_select_button: Button = $"Results/Level Select"
 
-var player_name = "XXXXX";
+var player_name = "";
 
 func _ready() -> void:
-	$"VBoxContainer/Level Select".grab_focus();
+	get_tree().get_first_node_in_group("splitscreen").queue_free()
+	results.focus_behavior_recursive = Control.FOCUS_BEHAVIOR_DISABLED
+	name_input_panel.focus_behavior_recursive = Control.FOCUS_BEHAVIOR_INHERITED
+	results.visible = false
+	name_input_panel.visible = true
+	d_key.grab_focus()
 	time_label.text = "Completion Time: " + str(TimeManager.return_singleplayer_time());
 	leaderboard_title.text = "Leaderboard";
 	leaderboard.text = "";
-	player_name = "XXXXX"; # default player name
+	player_name = ""
 
 	name_input_panel.show();
 
@@ -34,15 +41,16 @@ func set_leaderboard() -> void: ## Set the leaderboard with the new time
 				break;
 		leaderboard.text += player_name + " ----- " + str(leaderboard_values[i]) + "s\n";
 
+func _physics_process(delta: float) -> void:
+	player_name_label.text = player_name if player_name != "" else "Enter Name..."
+
 func _on_level_select_pressed() -> void:
 	var level_select: Control =  MenuStartScreen.LEVEL_SELECT.instantiate()
 	level_select.is_multiplayer = get_tree().get_first_node_in_group("splitscreen").current_session_info.is_multiplayer
 	get_tree().root.add_child(level_select)
-	get_tree().get_first_node_in_group("splitscreen").queue_free()
 	queue_free()
 
 func _on_quit_pressed() -> void:
-	get_tree().get_first_node_in_group("splitscreen").queue_free()
 	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
 	queue_free()
 
@@ -50,94 +58,97 @@ func _on_name_input_pressed() -> void:
 	name_input_panel.show();
 
 func _on_submit_button_pressed() -> void:
-	player_name = player_name_label.text;
-	name_input.text = player_name_label.text;
+	name_input.text = player_name
 	name_input_panel.hide();
+	results.focus_behavior_recursive = Control.FOCUS_BEHAVIOR_INHERITED
+	name_input_panel.focus_behavior_recursive = Control.FOCUS_BEHAVIOR_DISABLED
+	results.visible = true
+	name_input_panel.visible = false
 	level_select_button.grab_focus()
 	
 	set_leaderboard(); # submit leaderboard on name_input_pressed
 
 func _on_a_pressed() -> void:
-	player_name_label.text += "A";
+	player_name += "A";
 
 func _on_b_pressed() -> void:
-	player_name_label.text += "B";
+	player_name += "B";
 
 func _on_c_pressed() -> void:
-	player_name_label.text += "C";
+	player_name += "C";
 
 func _on_d_pressed() -> void:
-	player_name_label.text += "D";
+	player_name += "D";
 
 func _on_e_pressed() -> void:
-	player_name_label.text += "E";
+	player_name += "E";
 
 func _on_f_pressed() -> void:
-	player_name_label.text += "F";
+	player_name += "F";
 
 func _on_g_pressed() -> void:
-	player_name_label.text += "G";
+	player_name += "G";
 
 func _on_h_pressed() -> void:
-	player_name_label.text += "H";
+	player_name += "H";
 
 func _on_i_pressed() -> void:
-	player_name_label.text += "I";
+	player_name += "I";
 
 func _on_j_pressed() -> void:
-	player_name_label.text += "J";
+	player_name += "J";
 
 func _on_k_pressed() -> void:
-	player_name_label.text += "K";
+	player_name += "K";
 
 func _on_l_pressed() -> void:
-	player_name_label.text += "L";
+	player_name += "L";
 
 func _on_m_pressed() -> void:
-	player_name_label.text += "M";
+	player_name += "M";
 
 func _on_n_pressed() -> void:
-	player_name_label.text += "N";
+	player_name += "N";
 
 func _on_o_pressed() -> void:
-	player_name_label.text += "O";
+	player_name += "O";
 
 func _on_p_pressed() -> void:
-	player_name_label.text += "P";
+	player_name += "P";
 
 func _on_q_pressed() -> void:
-	player_name_label.text += "Q";
+	player_name += "Q";
 
 func _on_r_pressed() -> void:
-	player_name_label.text += "R";
+	player_name += "R";
 
 func _on_s_pressed() -> void:
-	player_name_label.text += "S";
+	player_name += "S";
 
 func _on_t_pressed() -> void:
-	player_name_label.text += "T";
+	player_name += "T";
 
 func _on_u_pressed() -> void:
-	player_name_label.text += "U";
+	player_name += "U";
 
 func _on_v_pressed() -> void:
-	player_name_label.text += "V";
+	player_name += "V";
 
 func _on_w_pressed() -> void:
-	player_name_label.text += "W";
+	player_name += "W";
 
 func _on_x_pressed() -> void:
-	player_name_label.text += "X";
+	player_name += "X";
 	
 func _on_y_pressed() -> void:
-	player_name_label.text += "Y";
+	player_name += "Y";
 
 func _on_z_pressed() -> void:
-	player_name_label.text += "Z";
+	player_name += "Z";
 
 
 func _on_delete_button_pressed() -> void:
-	player_name_label.text = player_name_label.text.left(player_name_label.text.length() - 1); # remove the last letter in the name
+	player_name = player_name.left(player_name.length() - 1); # remove the last letter in the name
 
 func _on_clear_button_pressed() -> void:
-	player_name_label.text = ""; # remove all characters in the name
+	player_name = ""; # remove all characters in the name
