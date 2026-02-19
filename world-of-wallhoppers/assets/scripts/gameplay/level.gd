fa10e5d50746812d70498122e68a8a739ec4bf6a
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2026-02-19 13:44:49 -0500

    Added Warnings for Incorrectly Setup Levels

--- world-of-wallhoppers/assets/scripts/gameplay/level.gd
+++ world-of-wallhoppers/assets/scripts/gameplay/level.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node2D
 class_name Level
 
@@ -7,8 +8,32 @@ class_name Level
 @onready var level_center: Marker2D = $LevelCenter
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		setup_editor()
+		return
 	assert(level_center != null, "Level does not have a \"Level Center\" Marker2D node as a direct child! This is required for every level")
 	assert(player_spawn_1 != null, "Level could not find Player 1's Spawn!\nMake sure there is a Node2D named \"PlayerSpawn1\" which is a direct child of the level!")
 	assert(player_spawn_1 != null, "Level could not find Player 2's Spawn!\nMake sure there is a Node2D named \"PlayerSpawn2\" which is a direct child of the level!")
+
+# Update warnings when any node postion or name changes in the children
+func setup_editor() -> void:
+	child_order_changed.connect(func(): update_configuration_warnings())
+	get_tree().node_renamed.connect(func(): update_configuration_warnings())
+
+# Generates warnings to ensure level is setup correctly
+func _get_configuration_warnings():
+	var warnings = []
 	
+	if $LevelCenter == null:
+		warnings.append("Level needs a Marker2D child named \"LevelCenter\"!")
+	if $PlayerSpawn1 == null:
+		warnings.append("Level needs a Marker2D child named \"PlayerSpawn1\"!")
+	if $PlayerSpawn2 == null:
+		warnings.append("Level needs a Marker2D child named \"PlayerSpawn2\"!")
+	if $ScrollStopBottom == null:
+		warnings.append("Level needs a Marker2D child named \"ScrollStopBottom\"!")
+	if $ScrollStopTop == null:
+		warnings.append("Level needs a Marker2D child named \"ScrollStopTop\"!")
 	
+	# Returning an empty array means "no warning".
+	return warnings
