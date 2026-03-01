9a8d3e42a40884c3212c7e560b72df4b55421040
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2026-03-01 11:02:22 -0500

    Added Log Shower with Highlighting

---
+++ world-of-wallhoppers/assets/scripts/debugging/debug_window/debug_logs.gd
@@ -0,0 +1,70 @@
+extends Control
+
+@onready var refresh: Button = $LogVBox/LogButtonsMargin/LogButtonHBox/Refresh
+@onready var scroll_down: Button = $LogVBox/LogButtonsMargin/LogButtonHBox/ScrollDown
+@onready var scroll_up: Button = $LogVBox/LogButtonsMargin/LogButtonHBox/ScrollUp
+
+@onready var log_text: RichTextLabel = $LogVBox/LogMargins/LogScrolling/LogText
+@onready var log_scrolling: ScrollContainer = $LogVBox/LogMargins/LogScrolling
+
+var scroll_speed: float = 10.0
+
+func _ready() -> void:
+	#refresh_log()
+	refresh.pressed.connect(refresh_log)
+
+func _physics_process(_delta: float) -> void:
+	if scroll_down.button_pressed:
+		log_scrolling.scroll_vertical += 1
+	if scroll_up.button_pressed:
+		log_scrolling.scroll_vertical -= 1
+
+func refresh_log() -> void:
+	var log_file: FileAccess = FileAccess.open("user://logs/godot.log", FileAccess.READ)
+	if log_file == null:
+		log_text.text = "Could not load logs"
+		return
+	log_text.text = log_file.get_as_text()
+	log_file.close()
+	highlight()
+
+func highlight() -> void:
+	log_text.text = highlight_word(log_text.text, "ERROR", Color(1.0, 0.339, 0.273, 1.0))
+	log_text.text = highlight_word(log_text.text, "WARNING", Color.ORANGE)
+	
+	log_text.text = highligh_between(log_text.text, '"', '"', Color.WHEAT)
+
+func highlight_word(text: String, word: String, color: Color) -> String:
+	var i: int = 0
+	var cycles: int = 0
+	while i < text.length():
+		cycles += 1
+		if cycles > 10000:
+			break
+		if text[i] == word[0]:
+			if text.substr(i, word.length()) == word:
+				var addition: String = "[color=" + color.to_html(false) + "]" + word + "[/color]"
+				text = text.substr(0, i) + addition + text.substr(i + word.length())
+				i += addition.length()
+		i += 1
+	return text
+
+func highligh_between(text: String, start: String, end: String, color: Color) -> String:
+	var highlighting: bool = false
+	var i: int = 0
+	while i < text.length():
+		
+		if text[i] == start[0] and not highlighting:
+			var addition: String = "[color=" + color.to_html() + "]"
+			text = text.insert(i, addition)
+			i += addition.length()
+			highlighting = true
+		elif text[i] == end[0] and highlighting:
+			var addition: String = "[/color]"
+			text = text.insert(i, addition)
+			i += addition.length()
+			highlighting = false
+		
+		i += 1
+	
+	return text
